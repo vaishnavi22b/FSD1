23dc8adc7247eaef3bd1b0f58d421fc9024695ef
Vaishnavi Tripathi <vaishnavitripathi2202@gmail.com>
2026-07-31 14:42:20 +0530

    Initial Commit

---
+++ react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card1 from './component/Card1'
+import Card2 from './component/Card2'
+import Card3 from './component/Card3'
+
+const App = () => {
+  return (
+    <div>
+      <Card1 />
+      <Card2/>
+      <Card3/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/src/component/Card2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card2 = () => {
+  return (
+    <div style={{ border: '4px solid red', height: '400px', width: '400px' ,backgroundColor: 'yellowgreen'}}>
+      <h3 style={{ color: 'red' }}>Book Shop</h3>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjoimR663HfAes4H0aaz-3DT5zAl4ZN6W2XBDdWwzJGg&s=10" alt="" height={'200px'} width={'200px'}/>
+    <h3>BOOK NAME: CHEMISTRY BOOK</h3>
+    <h3>600</h3>
+    </div>
+  )
+}
+
+export default Card2
