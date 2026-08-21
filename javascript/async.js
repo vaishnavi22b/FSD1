826cd4ce04b26c6e478ef83b84ee08f7db589247
Vaishnavi Tripathi <vaishnavitripathi2202@gmail.com>
2026-08-21 13:49:13 +0530

    async.js

---
+++ javascript/async.js
@@ -0,0 +1,8 @@
+console.log("task one");
+console.log("task two");
+setTimeout(() => {
+
+console.log("task three");
+}, 2000000);
+console.log("task four");
+console.log("task five");
---
+++ javascript/sync.js
@@ -0,0 +1,7 @@
+
+console.log("task one");
+for(let i=1;i<=1000000;i++) {
+    console.log(i)
+}
+console.log("second task");
+console.log("third task");
